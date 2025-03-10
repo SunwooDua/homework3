713cde034b01b518a8b966efeab4586b556cfccf
Sunwoo Yeom <o55306605@gmail.com>
2025-03-10 01:44:11 -0400

    added images / logic to save image state and limit dup

--- lib/main.dart
+++ lib/main.dart
@@ -19,21 +19,34 @@ class CardMatchApp extends StatelessWidget {
 }
 
 class grid_box extends StatefulWidget {
+  final frontCard card;
+  const grid_box({Key? key, required this.card}) : super(key: key);
+
   @override
   State<grid_box> createState() => _grid_boxState();
 }
 
 class _grid_boxState extends State<grid_box> {
   bool selected = false;
+
   final List<String> cardFronts = [
     'assets/card1.png',
     'assets/card2.png',
     'assets/card3.png',
     'assets/card4.png',
+    'assets/card5.png',
+    'assets/card6.png',
+    'assets/card7.png',
+    'assets/card8.png',
+
     'assets/card1.png',
     'assets/card2.png',
     'assets/card3.png',
     'assets/card4.png',
+    'assets/card5.png',
+    'assets/card6.png',
+    'assets/card7.png',
+    'assets/card8.png',
   ]; // card list for front, put two since need matching
 
   String randomFront = ''; // save random image
@@ -43,9 +56,7 @@ class _grid_boxState extends State<grid_box> {
       onTap: () {
         setState(() {
           selected = !selected;
-          if (selected) {
-            randomFront = cardFronts[Random().nextInt(cardFronts.length)];
-          }
+          widget.card.isFlipped = selected;
         });
       },
       child: Padding(
@@ -58,7 +69,7 @@ class _grid_boxState extends State<grid_box> {
           curve: Curves.bounceInOut,
           child:
               selected
-                  ? Image(image: AssetImage(randomFront))
+                  ? Image(image: AssetImage(widget.card.frontImage))
                   : Image(image: AssetImage('assets/card_back.png')),
         ),
       ),
@@ -66,14 +77,50 @@ class _grid_boxState extends State<grid_box> {
   }
 }
 
-class CardMatchScreen extends StatefulWidget {
-  const CardMatchScreen({super.key});
+class frontCard {
+  //track if cards are flippped
+  final String frontImage;
+  bool isFlipped;
 
+  frontCard({required this.frontImage, this.isFlipped = false});
+}
+
+class CardMatchScreen extends StatefulWidget {
   @override
   State<CardMatchScreen> createState() => _CardMatchScreenState();
 }
 
 class _CardMatchScreenState extends State<CardMatchScreen> {
+  List<frontCard> cards = [];
+
+  // initialzation of cards (random and check if flipped) use initState
+  void initState() {
+    super.initState();
+
+    cards = [
+      frontCard(frontImage: 'assets/card1.png'),
+      frontCard(frontImage: 'assets/card2.png'),
+      frontCard(frontImage: 'assets/card3.png'),
+      frontCard(frontImage: 'assets/card4.png'),
+      frontCard(frontImage: 'assets/card5.png'),
+      frontCard(frontImage: 'assets/card6.png'),
+      frontCard(frontImage: 'assets/card7.png'),
+      frontCard(frontImage: 'assets/card8.png'),
+
+      frontCard(frontImage: 'assets/card1.png'),
+      frontCard(frontImage: 'assets/card2.png'),
+      frontCard(frontImage: 'assets/card3.png'),
+      frontCard(frontImage: 'assets/card4.png'),
+      frontCard(frontImage: 'assets/card5.png'),
+      frontCard(frontImage: 'assets/card6.png'),
+      frontCard(frontImage: 'assets/card7.png'),
+      frontCard(frontImage: 'assets/card8.png'),
+    ];
+
+    //randomize
+    cards.shuffle();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,7 +134,7 @@ class _CardMatchScreenState extends State<CardMatchScreen> {
           crossAxisCount: 4,
         ),
         itemBuilder: (context, index) {
-          return grid_box();
+          return grid_box(card: cards[index]);
         },
       ),
     );
